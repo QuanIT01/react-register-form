28ccd489cdfa965a2740be229236feed46ca9517
QuanIT01 <n.h.quan126@gmail.com>
2023-05-01 14:34:56 +0700

    react form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import RegisterHook from "./components/form/RegisterHook";
+
+import RegistetFormik from "./components/form/RegistetFormik";
+
+function App() {
+  return (
+    <div className="App">
+      <RegistetFormik></RegistetFormik>
+    </div>
+  );
+}
+
+export default App;
